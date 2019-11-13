23322550f2dcb4db120c5961bda2e630b35bae89
adzguy <dnurmeto@neiu.edu>
2019-11-13 00:51:15 -0600

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Davron on 11/13/19.
+//  Copyright © 2019 Davron. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
